1f1f797feaf438cc041e54ba6539a189611eeb37
Robert Pergl <robert@pergl.org>
2019-12-23 16:52:31 +0100

    generateScript simplification

--- scripts/generateSchemas.sh
+++ scripts/generateSchemas.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 D="app"
+AF="$D/annotationsModel.ts"
 O="--topRef --noExtraProps"
 
 S="anRecord.schema.js"
 F="$D/$S"
 echo "export const anRecordSchema = " > $F
-typescript-json-schema --id "anRecord" $O --required $D/annotationsModel.ts AnRecord >> $F
+typescript-json-schema --id "anRecord" $O --required $AF AnRecord >> $F
 
 # Schema with optional fields for patch validations
 S="anRecord.opt.schema.js"
 F="$D/$S"
 echo "export const anRecordOptSchema = " > $F
-typescript-json-schema --id "anRecordOpt" $O $D/annotationsModel.ts AnRecord >> $F
+typescript-json-schema --id "anRecordOpt" $O $AF AnRecord >> $F
 
 S="getQuery.schema.js"
 F=$D/$S
 echo "export const getQuerySchema = " > $F
-typescript-json-schema --id "getQuery" $O --required $D/annotationsModel.ts GetQuery >> $F
+typescript-json-schema --id "getQuery" $O --required $AF GetQuery >> $F
 
 S="targetsQuery.schema.js"
 F=$D/$S
 echo "export const targetsQuerySchema = " > $F
-typescript-json-schema --id "targetsQuery" $O --required $D/annotationsModel.ts TargetsQuery >> $F
+typescript-json-schema --id "targetsQuery" $O --required $AF TargetsQuery >> $F
