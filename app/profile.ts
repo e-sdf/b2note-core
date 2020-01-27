5528da50b068a5b5783e90cec8709337cc27ea64
Robert Pergl <robert@pergl.org>
2020-01-27 21:33:36 +0100

    profile update

--- app/profile.ts
+++ app/profile.ts
@@ -1,6 +1,11 @@
-export interface User {
+export interface UserProfile {
   id: string;
   name: string;
+  nickname: string;
   email: string;
+  organisation: string;
+  jobTitle: string;
+  country: string;
+  experience: string;
   accessToken: string;
 }
