9acf7fb70680c3b73c6a2c55577acf80e12195f9
Robert Pergl <robert@pergl.org>
2020-08-28 16:35:48 +0200

    add nanopub scripts

--- scripts/generateSchemas.sh
+++ scripts/generateSchemas.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ./scripts/generateAnnotationSchemas.sh
+./scripts/generateNanopubSchemas.sh
 ./scripts/generateHandleSchemas.sh
 ./scripts/generateProfileSchemas.sh
 ./scripts/generateQuerySchemas.sh
