cdfd64084065d618739f14d361d01e5f55b2b346
Robert Pergl <robert@pergl.org>
2020-08-30 19:23:41 +0200

    move generate scripts out of core

--- scripts/generateSchemas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./scripts/generateAnnotationSchemas.sh
-./scripts/generateNanopubSchemas.sh
-./scripts/generateHandleSchemas.sh
-./scripts/generateProfileSchemas.sh
-./scripts/generateAnQuerySchemas.sh
-./scripts/generateNpQuerySchemas.sh
