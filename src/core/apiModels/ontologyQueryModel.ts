3610e3f211ae962bc77afd357d82e064646044de
Robert Pergl <robert@pergl.org>
2020-10-12 21:12:32 +0200

    add OntologySources

--- src/core/apiModels/ontologyQueryModel.ts
+++ src/core/apiModels/ontologyQueryModel.ts
@@ -1,5 +1,17 @@
+import type { Ontology } from "../ontologyRegister";
+
+export interface OntologySources {
+  solr: boolean;
+  custom: Array<Ontology>;
+}
+
+export const defaultOntologySources: OntologySources = {
+  solr: true,
+  custom: []
+};
+
 export interface OntologyQuery {
   value?: string;
   uri?: string;
-  sources?: Array<string>;
+  sources?: OntologySources;
 }
