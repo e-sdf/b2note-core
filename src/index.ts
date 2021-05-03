4aa23157e4271dd5d516103c7e571e3f1fc2a6e5
Louis Vinchon <louis.vinchon.dev@gmail.com>
2021-05-03 12:24:27 +0200

    Add library index (src/index.ts)

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import * as annotationsModel from "./core/annotationsModel";
+import * as domainModel from "./core/domainModel";
+import * as formats from "./core/formats";
+import * as http from "./core/http";
+import * as ontologyRegister from "./core/ontologyRegister";
+import * as searchModel from "./core/searchModel";
+import * as searchQueryParser from "./core/searchQueryParser";
+import * as tagRecord from "./core/tagRecord";
+import * as targetInput from "./core/targetInput";
+import * as user from "./core/user";
+import * as utils from "./core/utils";
+
+export {
+  annotationsModel,
+  domainModel,
+  formats,
+  http,
+  ontologyRegister,
+  searchModel,
+  searchQueryParser,
+  tagRecord,
+  targetInput,
+  user,
+  utils,
+};
+
